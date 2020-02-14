512c1d533e5bd17ebc439f8aeed7be0dfd984bc6
kinako555 <dodohunter_3658@yahoo.co.jp>
2020-02-15 02:15:36 +0900

    終了

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SelectionController@home');
+Route::resource('selections', 'SelectionController', [
+    'only' => ['create', 'update', 'destroy']
+  ]);
+
